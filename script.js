fe679037783ead2cdaa0ba0670f143330cc1ec14
xnilzzz <95069211+ArkBlue24@users.noreply.github.com>
2025-08-15 18:00:38 +0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav button').forEach(btn => {
-  btn.addEventListener('click', () => {
-    const target = btn.getAttribute('data-target');
-    document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
-    document.getElementById(target).classList.add('active');
-  });
-});
